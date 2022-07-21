e55aa159c75a06fe9fabe4e44665fb99d9040b1e
lisandrosalast <109685186+lisandrosalast@users.noreply.github.com>
2022-07-21 14:46:51 -0400

    Delete public directory

--- public/modulo/conectar.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","id18957089_u","uj6@*u7uzN2eZbE+","id18957089_db");
-//$con = mysqli_connect("localhost","root","","id14118178_db");
-$con->set_charset("utf8");
-?>
--- public/modulo/error.php
+++
@@ -1,8 +0,0 @@
-<div class='a25'></div><div class='a50'>
-<h2>¡Ha ocurrido un error!</h2>
-<p>No se ha podido encontrar el recurso solicitado.<br/><br/>
-Puede hacer lo siguiente:</p>
-<p><i>1) Verifique que la dirección esté escrita correctamente.</i></p>
-<p><i>2) Si considera que lo que busca debe estar aquí, llámenos para preguntar por qué no está.</i></p>
-</div><div class='a25'></div>
-<!-- META HTTP-EQUIV="REFRESH" CONTENT="0;URL=http://lisandrosalast.000webhostapp.com"--> 
